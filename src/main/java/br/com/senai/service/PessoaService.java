e61ac3955e8712b45857277228bf529dd1ea9302
Manerich <e.manerich@hotmal.com>
2024-10-21 19:23:00 -0300

    PessoaService

--- src/main/java/br/com/senai/service/PessoaService.java
+++ src/main/java/br/com/senai/service/PessoaService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+// Aula top começamos arrumando o PessoaService
 
 @Service
 public class PessoaService {
